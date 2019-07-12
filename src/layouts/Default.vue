435df4588f50ade0472cde0093b87513a60462d3
zkriszti <zavecz.krisztina@gmail.com>
2019-07-12 12:29:09 +0200

    replace hardcoded menu with component

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,14 +4,21 @@
       <strong>
         <g-link to="/">{{ $static.metaData.siteName }}</g-link>
       </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about">About</g-link>
-      </nav>
+      <MainNav />
     </header>
     <slot/>
   </div>
 </template>
+
+<script>
+  import MainNav from '../components/MainNav.vue'
+
+export default {
+  components: {
+    MainNav
+  }
+}
+</script>
 
 <static-query>
 query {
@@ -21,30 +28,44 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
+<style lang="stylus">
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
+body
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif
+  margin:0
+  padding:0
+  line-height: 1.5
+  color: #444
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
+.layout
+  max-width: 760px
+  margin: 0 auto
+  padding-left: 20px
+  padding-right: 20px
 
-.nav__link {
-  margin-left: 20px;
-}
+.header
+  display: flex
+  justify-content: space-between
+  align-items: center
+  margin-bottom: 20px
+  height: 80px
+  font-family: Comfortaa, sans-serif
+  a
+    text-decoration: none
+    text-transform: lowercase
+    &:visited
+      color: #444
+    &.nav__link
+      margin-left: 20px
+      padding: 6px 12px
+      border-radius: 4px
+      background: #eee
+
+img
+  max-width: 100%
+
+pre, p code
+  background: #eeeeee
+  padding: 8px 10px
+
 </style>
