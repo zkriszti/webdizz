070ed29e53d76e823d1d8a786f91ba12a38c55f5
zkriszti <zavecz.krisztina@gmail.com>
2019-07-12 12:31:46 +0200

    blog aggregate page starting out

---
+++ src/pages/Blog.vue
@@ -0,0 +1,40 @@
+<template>
+  <Layout>
+    <h1>Blog</h1>
+    <p>this is the blog page</p>
+    <div v-for="edge in $page.posts.edges" :key="edge.node.id">
+      {{ edge.node.id }}
+      <span>{{ edge.node.date }}</span>
+      <g-link :to="edge.node.path"> {{ edge.node.title }} </g-link>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Posts {
+  posts: allPost {
+    edges {
+      node {
+        path
+        id
+        title
+        date
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Blog'
+  }
+}
+</script>
+
+<style scoped>
+  span {
+    background: #e6e6e6
+  }
+</style>
