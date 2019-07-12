3f28c8520a7041ce67344843b84d3fbbd6cc757e
zkriszti <zavecz.krisztina@gmail.com>
2019-07-12 12:34:21 +0200

    nav component

---
+++ src/components/MainNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav class="nav">
+      <g-link class="nav__link" v-for="(item, index) in menuItems" :to="item.target" :key="index">
+        {{ item.name }}
+      </g-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menuItems: [
+        {name: 'Blog', target: "/blog"}
+      ]
+    }
+  }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
