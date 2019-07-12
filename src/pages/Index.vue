0cc78c156b88623d8f7ffca51c6cd679273bc5bc
zkriszti <zavecz.krisztina@gmail.com>
2019-07-12 12:29:37 +0200

    initial commit content for main page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,18 +2,27 @@
   <Layout>
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    <g-image alt="Krisztina Závecz" src="../../uploads/krisztizavecz.png" width="135" />
 
-    <h1>Hello, world!</h1>
+    <h1>Get to know me:</h1>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+    <div>
+      <p>I'm a Frontend Developer living in Budapest, Hungary.</p>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+      <p>I love creating, I find joy in revealing pieces of knowledge I haven't had before. I love the feeling of finally resolving a bug. :) I love CSS.</p>
+
+      <p>My current technology stack includes daily work with the following:</p>
+      <ul>
+        <li>Responsive frontend technologies - HTML5 - CSS3</li>
+        <li>Vue.JS and native Javascript</li>
+      </ul>
+
+      <p>For the records, I also have prior experience with:</p>
+      <ul>
+        <li>frontend work for PHP-based backends</li>
+        <li>frontend work with MS SharePoint 2013 / 2016 (JSOM, unique branded layouts, etc.)</li>
+      </ul>
+    </div>
 
   </Layout>
 </template>
