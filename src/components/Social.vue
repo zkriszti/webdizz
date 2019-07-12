3d88105c1f1ae73ce526c4f639a3db178fe35a57
zkriszti <zavecz.krisztina@gmail.com>
2019-07-13 00:52:56 +0200

    add social icons component

---
+++ src/components/Social.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <g-link v-for="(item, index) in iconItems" :to="item.target" :key="index">
+        <font-awesome :icon="item.icon"/>
+      </g-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      iconItems: [
+        {name: 'Twitter', target: "https://twitter.com/krisztizavecz", icon: ['fab', 'twitter']},
+        {name: 'GitHub', target: "https://github.com/zkriszti", icon: ['fab', 'github']},
+        {name: 'DevTo', target: "https://dev.to/zkriszti", icon: ['fab', 'dev']}
+      ]
+    }
+  }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
