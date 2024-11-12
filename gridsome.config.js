9a7d40b2365f7cb7a8e8e04704ebfb70df79cb1a
Krisztina Závecz <krisztina.zavecz@memoq.com>
2024-11-12 10:20:28 +0100

    fix RSS

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
+  siteUrl: "https://krisztinazavecz.com",
   siteName: "HTML, CSS, JavaScript and related technologies",
 
   port: 8200,
