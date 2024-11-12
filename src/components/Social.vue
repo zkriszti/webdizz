8fb89b6a52414dc5d1d06f5a5848a5860e6731e5
Krisztina Závecz <krisztina.zavecz@memoq.com>
2024-11-12 10:07:04 +0100

    social: add bluesky, replace twitter

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,47 +1,51 @@
 <template>
   <div class="social">
-      <a v-for="(item, index) in iconItems" :href=item.target :key="index" :aria-label=item.aria>
-        <font-awesome :icon="item.icon"/>
-      </a>
+    <a
+      v-for="(item, index) in iconItems"
+      :href="item.target"
+      :key="index"
+      :aria-label="item.aria"
+    >
+      <font-awesome :icon="item.icon" />
+    </a>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       iconItems: [
         {
-          name: 'Twitter',
-          target: "https://twitter.com/krisztizavecz",
-          icon: ['fab', 'twitter'],
-          aria: 'follow me on twitter'
+          name: "Bluesky",
+          target: "https://bsky.app/profile/krisztinazavecz.com",
+          icon: ["fab", "bluesky"],
+          aria: "follow me on bluesky",
         },
         {
-          name: 'GitHub',
+          name: "GitHub",
           target: "https://github.com/zkriszti",
-          icon: ['fab', 'github'],
-          aria: 'my github account'
+          icon: ["fab", "github"],
+          aria: "my github account",
         },
         {
-          name: 'DevTo',
+          name: "DevTo",
           target: "https://dev.to/zkriszti",
-          icon: ['fab', 'dev'],
-          aria: 'follow me on dev.to'
-        }
-      ]
-    }
-  }
-}
-
+          icon: ["fab", "dev"],
+          aria: "follow me on dev.to",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
-  a
-    margin-right: 10px
-  a svg
-    max-width: 18px;
-  a path
-  a:visited path
-    fill: #eee
+a
+  margin-right: 10px
+a svg
+  max-width: 18px;
+a path
+a:visited path
+  fill: #eee
 </style>
